fd42668df2718c71bcaba437efae11387dcf4637
DESKTOP-FDRPE67\rk98 <roma.cuznetso2010@gmail.com>
2026-03-15 13:57:47 +0400

    добавил блок для сео, исправил текста с учетом сео оптимизации

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,14 +1,15 @@
 export interface IServiceItem {
   title: string;
-  description: string;
+  text: string;
   price: number;
   imageUrl: string;
   propertyUrl: string;
+  seo?: ISeoProps;
 }
 
 export interface IServiceCategory {
   title: string;
-  description: string;
+  text: string;
   imageUrl: string;
   propertyUrl: string;
   items: IServiceItem[];
@@ -21,7 +22,7 @@ export interface IPageData {
 // Полная типизация Service.md
 export interface IPageDataProps {
     title: string;
-    description: string;
+    text: string;
     seo: ISeoProps;
     services: IServiceCategory[];
 }
@@ -30,4 +31,5 @@ export interface ISeoProps {
     title: string;
     description: string;
     ogImage: string;
+    keywords: string[];
 }
